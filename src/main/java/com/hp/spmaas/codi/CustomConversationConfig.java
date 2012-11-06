530fa10a73c6c6d8ce125ac7386d7c3224b62430
gongy <topikachu@163.coml>
2012-11-06 22:04:50 +0800

    Fix a weld issue, see https://issues.jboss.org/browse/WELD-1247 Disable the windowID generate from CODI which causes many redirect. see http://ocpsoft.org/support/prettyfaces-users/beginner-question-regarding-comocpsoftvp_0 Remove jsf listener in web.xml which is registered automatically

---
+++ src/main/java/com/hp/spmaas/codi/CustomConversationConfig.java
@@ -0,0 +1,32 @@
+package com.hp.spmaas.codi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Specializes;
+
+import org.apache.myfaces.extensions.cdi.core.api.config.ConfigEntry;
+import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.config.ConversationConfig;
+import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.config.WindowContextConfig;
+
+@Specializes
+@ApplicationScoped
+public class CustomConversationConfig extends WindowContextConfig
+{
+
+	@Override
+	@ConfigEntry
+	public boolean isAddWindowIdToActionUrlsEnabled() {
+		
+		return false;
+	}
+
+	@Override
+	@ConfigEntry
+	public boolean isUrlParameterSupported() {
+		
+		return false;
+	}
+	
+	
+   
+}
